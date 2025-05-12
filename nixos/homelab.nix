17c65b135ca965f436638bcd85ebd70bf0bb1107
death916 <tavn1992@gmail.com>
2025-05-12 05:28:42 +0000

    audio booksheld and users

--- nixos/homelab.nix
+++ nixos/homelab.nix
@@ -55,7 +55,7 @@
   # Define the 'death916' user for the server
   users.users.death916 = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ]; # For sudo access
+    extraGroups = [ "wheel" "media-services"]; # For sudo access
     openssh.authorizedKeys.keys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCte9KjJUSn4xBPEKCk9QER6+jF+C0uBatVl27zIamYsryyHdFrmqK2DAg7OhqTHqzjxID6sp6d57MsJqOUAtwXbUDMLffqBSerUxfTm+1SPDrhL0GSvo0QVjMLVseOEq8d2qXgW1R7dIk412VbO5e9SAump5aJOHI/SzT6VLoUczalmqrjnDacWQMeLu/TSOZHcfrhjYSg+b1xbc1vHp6C4obOb8JIj/anAieT/1P36MhlNW79ow6PWenLemBYeeezFrKtESF1oMc8jmcxogzgLamlqhKYAHlKhOuBF6u0nRneI5IPDbbMF5zwEv5szCEKj8XZJVYUk8uUg7ARyppjcA7yAXuaNKBNxa7tfjqWrDWOACn97ufE5FFJt0XH5JzkXcDh96K8ZSZaWxMRu2s+GlIu/1F415xtVfe1d79HYkWke/ewaQ4NqgOt8f7wRvyzabpQZDzkaXO0UoK65O2HyUur33XWCEmV+1pB6BrS8pD+1I4Tvbnc+rOgtHTTRfKqezKqZmaErEOxClBwvWjvn0PzhGSoClTGXPjhl239/sH0JGY09dTBh8GtAVbfv+jFO6nm6aR7O/OwSaohY3uOdRo8XyxJr4XyGAaBNRdm6BUJRnB4W51J49IQBZzIe2NUkNMHeUT4jkxFpfhkujnSFw2ZnOLkERpwkltAlbwuLw== tavn1992@gmail.com" # <<-- REPLACE THIS WITH YOUR SSH PUBLIC KEY for death916
       # Add more keys if needed
@@ -63,6 +63,14 @@
     # If 'death916' needs a password on the server (less secure than key-only)
     # initialPassword = "yoursecurepassword"; # Or use hashed password
   };
+
+  users.users.audiobookshelf = {
+    isSystemUser = true; # System user, doesn't need a home directory by default for services
+    group = "media_services"; # Primary group
+    # extraGroups = [ "media_services" ]; # Alternatively, if you want a different primary group
+  };
+
+  users.groups.media_services = {};
 
   # homelab services
 
@@ -72,7 +80,12 @@
   user="death916";
   };
 
+  services.audiobookshelf = {
+    enable = true;
+    user = "audiobookshelf";
+    group = "media-services"
 
+  };
 
   # Sudo access for the wheel group (which death916 is part of)
   security.sudo.wheelNeedsPassword = true; # Or false if you prefer passwordless sudo for wheel
