4482c5228d5ad11477beb4280fac7b4c6139de43
death916 <mail@trentnelson.dev>
2025-05-14 07:37:25 -0700

    reset

--- overlays/halloy-overlay.nix
+++ overlays/halloy-overlay.nix
@@ -1,17 +1,13 @@
 self: super: {
-  halloy = super.halloy.overrideAttrs (oldAttrs: rec {
+  halloy = super.halloy.overrideAttrs (oldAttrs: {
     version = "2025.5";
     src = super.fetchFromGitHub {
       owner = "squidowl";
       repo = "halloy";
       rev = "2025.5";
-      sha256 = "sha256-cG/B6oiRkyoC5fK7bLdCDQYZymfMZspWXvOkqpwHRPk="; # Will get proper hash from error
+      sha256 = "sha256-cG/B6oiRkyoC5fK7bLdCDQYZymfMZspWXvOkqpwHRPk="; # ← Replace after build
     };
-    cargoDeps = oldAttrs.cargoDeps.overrideAttrs (super.lib.const {
-      name = "${pname}-${version}-vendor";
-      inherit src;
-      outputHash = "0000000000000000000000000000000000000000000000000000"; # Will get proper hash from error
-    });
+    cargoHash = ""; # ← Let Nix calculate this
   });
 }
 
