9e7a72daab6565df17934b04eb1df9c90b638626
death916 <mail@trentnelson.dev>
2025-05-14 07:40:15 -0700

    reset

--- overlays/halloy-overlay.nix
+++ overlays/halloy-overlay.nix
@@ -1,13 +1,31 @@
 self: super: {
-  halloy = super.halloy.overrideAttrs (oldAttrs: {
-    version = "2025.5";
-    src = super.fetchFromGitHub {
-      owner = "squidowl";
-      repo = "halloy";
-      rev = "2025.5";
-      sha256 = "sha256-cG/B6oiRkyoC5fK7bLdCDQYZymfMZspWXvOkqpwHRPk="; # ← Replace after build
-    };
-    cargoHash = ""; # ← Let Nix calculate this
-  });
+  halloy = super.callPackage ({ rustPlatform, fetchFromGitHub }:
+    rustPlatform.buildRustPackage rec {
+      pname = "halloy";
+      version = "2025.5";
+      
+      src = fetchFromGitHub {
+        owner = "squidowl";
+        repo = pname;
+        rev = version;
+        sha256 = "sha256-cG/B6oiRkyoC5fK7bLdCDQYZymfMZspWXvOkqpwHRPk="; # Keep this for now
+      };
+
+      cargoHash = ""; # Let Nix calculate this
+
+      # Add required build inputs based on original error logs
+      nativeBuildInputs = with super; [ pkg-config ];
+      
+      buildInputs = with super; [
+        libxkbcommon
+        openssl
+        vulkan-loader
+        xorg.libX11
+        xorg.libXcursor
+        xorg.libXi
+        xorg.libXrandr
+        wayland
+      ];
+    }) {};
 }
 
