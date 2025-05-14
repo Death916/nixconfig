c48f3f1b29bb3fdac5b13a57eb32b850f7ba7147
death916 <mail@trentnelson.dev>
2025-05-14 09:00:12 -0700

    hash

--- overlays/halloy-overlay.nix
+++ overlays/halloy-overlay.nix
@@ -46,4 +46,32 @@ in {
       outputHashes = {
         "cryoglyph-0.1.0" = "sha256-X7S9jq8wU6g1DDNEzOtP3lKWugDnpopPDBK49iWvD4o=";
         "dark-light-2.0.0" = "sha256-e826vF7iSkGUqv65TXHBUX04Kz2aaJJEW9f7JsAMaXE=";
+        "iced-0.14.0-dev" = "sha256-FEGk1zkXM9o+fGMoDtmi621G6pL+Yca9owJz4q2Lzks=";
+        "winit-0.30.8" = "sha256-hlVhlQ8MmIbNFNr6BM4edKdZbe+ixnPpKm819zauFLQ=";
+        "dpi-0.1.1" = "sha256-hlVhlQ8MmIbNFNr6BM4edKdZbe+ixnPpKm819zauFLQ=";
+      };
+    };
+
+    # System dependencies
+    nativeBuildInputs = with super; [ pkg-config ];
+    
+    buildInputs = with super; [
+      libxkbcommon
+      openssl
+      vulkan-loader
+      xorg.libX11
+      xorg.libXcursor
+      xorg.libXi
+      xorg.libXrandr
+      wayland
+    ];
+
+    meta = with super.lib; {
+      description = "Halloy IRC Client";
+      homepage = "https://github.com/squidowl/halloy";
+      license = licenses.gpl3Only;
+      platforms = platforms.linux;
+    };
+  };
+}
 
