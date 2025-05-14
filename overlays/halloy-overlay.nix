5b97b84aa32fd80b0c6d613ede9295ab93b402d1
death916 <mail@trentnelson.dev>
2025-05-14 08:31:22 -0700

    hash

--- overlays/halloy-overlay.nix
+++ overlays/halloy-overlay.nix
@@ -57,6 +57,9 @@ in {
         # You will get an error for this placeholder.
         # Replace it with the actual hash provided in the error message.
         "dpi-0.1.1" = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+        "winit-0.29.15" = "sha256-hlVhlQ8MmIbNFNr6BM4edKdZbe+ixnPpKm819zauFLQ=";
+
+        "
       };
     };
 
