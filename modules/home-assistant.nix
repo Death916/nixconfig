9769c631604d478b4923feba18e81fde09871159
death916 <tavn1992@gmail.com>
2025-05-15 12:14:12 +0000

    ha unstable

--- modules/home-assistant.nix
+++ modules/home-assistant.nix
@@ -1,16 +1,34 @@
+# /home/death916/nixconfig/modules/home-assistant.nix
+{ config, pkgs, lib, unstablePkgsHA, ... }: # Added unstablePkgsHA
+
 {
   services.home-assistant = {
     enable = true;
+    package = unstablePkgsHA.home-assistant; # Use HA package from unstable
+
     extraComponents = [
-      # Components required to complete the onboarding
       "esphome"
       "met"
       "radio_browser"
     ];
     config = {
-      # Includes dependencies for a basic setup
-      # https://www.home-assistant.io/integrations/default_config/
       default_config = {};
+      homeassistant = {
+        name = "Homelab HA (Unstable)";
+        
+        time_zone = "America/Los_Angeles";
+      };
+      # Add other integrations as needed
     };
   };
+
+  # Firewall rule for Home Assistant
+  networking.firewall.allowedTCPPorts = [ 8123 ];
+
+  # Ensure the `hass` user has access to necessary devices (e.g., Zigbee/Z-Wave dongles)
+  users.users.hass.extraGroups = lib.mkIf (config.services.home-assistant.enable) [
+    "dialout" # Common group for serial devices
+    # Add other groups if needed
+  ];
+
 }
