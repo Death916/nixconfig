437e8f798a7f562435e1b11abd75bda2ba173181
death916 <mail@trentnelson.dev>
2025-05-20 05:00:11 -0700

    flake

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -88,12 +88,12 @@ in
   };
   
   programs.starship = {
-  enable = true;
-  settings = {
-    add_newline = false;
-    aws.disabled = true;
-    gcloud.disabled = true;
-    line_break.disabled = true;
+    enable = true;
+    settings = {
+      add_newline = false;
+      aws.disabled = true;
+      gcloud.disabled = true;
+      line_break.disabled = true;
 
     conda = {
       truncation_length = 1;
