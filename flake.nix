953057d1302906122f306d478025ff0b3f668073
death916 <mail@trentnelson.dev>
2025-05-20 00:16:20 -0700

    25.05

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   description = "NixOS configurations for laptop and homelab server";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable"; # Added for Home Assistant
 
     nixos-cosmic = {
