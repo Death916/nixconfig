b6cc3607bfbf0cc2fcef789cdc4dd3e15da2e99b
death916 <mail@trentnelson.dev>
2025-08-06 03:35:35 -0700

    vaultwarden orac

--- modules/nixos/orac/services.nix
+++ modules/nixos/orac/services.nix
@@ -3,30 +3,30 @@
   pkgs,
   ...
 }:
+{
 
-  
   services.vaultwarden = {
     enable = true;
     backupDir = "/var/lib/vaultwarden/backup";
     config = {
-        # Refer to https://github.com/dani-garcia/vaultwarden/blob/main/.env.template
-        DOMAIN = "vaultwarden.death916.xyz";
-        SIGNUPS_ALLOWED = false;
-        
-        ROCKET_ADDRESS = "127.0.0.1";
-        ROCKET_PORT = 8222;
-        ROCKET_LOG = "critical";
+      # Refer to https://github.com/dani-garcia/vaultwarden/blob/main/.env.template
+      DOMAIN = "vaultwarden.death916.xyz";
+      SIGNUPS_ALLOWED = false;
 
-        # This example assumes a mailserver running on localhost,
-        # thus without transport encryption.
-        # If you use an external mail server, follow:
-        #   https://github.com/dani-garcia/vaultwarden/wiki/SMTP-configuration
-        # SMTP_HOST = "127.0.0.1";
-        # SMTP_PORT = 25;
-        # SMTP_SSL = false;
+      ROCKET_ADDRESS = "127.0.0.1";
+      ROCKET_PORT = 8222;
+      ROCKET_LOG = "critical";
 
-        # SMTP_FROM = "admin@bitwarden.example.com";
-        # SMTP_FROM_NAME = "example.com Bitwarden server";
+      # This example assumes a mailserver running on localhost,
+      # thus without transport encryption.
+      # If you use an external mail server, follow:
+      # https://github.com/dani-garcia/vaultwarden/wiki/SMTP-configuration
+      # SMTP_HOST = "127.0.0.1";
+      # SMTP_PORT = 25;
+      # SMTP_SSL = false;
+
+      # SMTP_FROM = "admin@bitwarden.example.com";
+      # SMTP_FROM_NAME = "example.com Bitwarden server";
     };
   };
 
