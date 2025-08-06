77837b236a3e8512cb003009d9271efcedd36e54
death916 <mail@trentnelson.dev>
2025-08-06 03:26:10 -0700

    vaultwarden orac

--- modules/nixos/orac/services.nix
+++ modules/nixos/orac/services.nix
@@ -3,7 +3,32 @@
   pkgs,
   ...
 }:
-{
+
+  
+  services.vaultwarden = {
+    enable = true;
+    backupDir = "/var/lib/vaultwarden/backup";
+    config = {
+        # Refer to https://github.com/dani-garcia/vaultwarden/blob/main/.env.template
+        DOMAIN = "vaultwarden.death916.xyz";
+        SIGNUPS_ALLOWED = false;
+        
+        ROCKET_ADDRESS = "127.0.0.1";
+        ROCKET_PORT = 8222;
+        ROCKET_LOG = "critical";
+
+        # This example assumes a mailserver running on localhost,
+        # thus without transport encryption.
+        # If you use an external mail server, follow:
+        #   https://github.com/dani-garcia/vaultwarden/wiki/SMTP-configuration
+        # SMTP_HOST = "127.0.0.1";
+        # SMTP_PORT = 25;
+        # SMTP_SSL = false;
+
+        # SMTP_FROM = "admin@bitwarden.example.com";
+        # SMTP_FROM_NAME = "example.com Bitwarden server";
+    };
+  };
 
   virtualisation.docker.enable = true;
 
