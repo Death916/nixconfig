69540077a0e57bc3b9b38c18526fd78e199f68ce
death916 <mail@trentnelson.dev>
2025-08-06 05:43:34 -0700

    rm vault

--- modules/nixos/orac/services.nix
+++ modules/nixos/orac/services.nix
@@ -5,32 +5,6 @@
 }:
 {
 
-  services.vaultwarden = {
-    enable = true;
-    dbBackend = "mysql";
-    #    backupDir = "/var/lib/backups";
-    config = {
-      # Refer to https://github.com/dani-garcia/vaultwarden/blob/main/.env.template
-      DOMAIN = "https://vaultwarden.death916.xyz";
-      SIGNUPS_ALLOWED = true;
-
-      ROCKET_ADDRESS = "0.0.0.0";
-      ROCKET_PORT = 8222;
-      ROCKET_LOG = "critical";
-
-      # This example assumes a mailserver running on localhost,
-      # thus without transport encryption.
-      # If you use an external mail server, follow:
-      # https://github.com/dani-garcia/vaultwarden/wiki/SMTP-configuration
-      # SMTP_HOST = "127.0.0.1";
-      # SMTP_PORT = 25;
-      # SMTP_SSL = false;
-
-      # SMTP_FROM = "admin@bitwarden.example.com";
-      # SMTP_FROM_NAME = "example.com Bitwarden server";
-    };
-  };
-
   virtualisation.docker.enable = true;
 
   systemd.services.kopia-backup = {
