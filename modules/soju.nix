a0eaa044ec20cf56a88c359f1d84aaecbe139464
death916 <mail@trentnelson.dev>
2025-08-07 03:24:47 -0700

    soju

--- modules/soju.nix
+++ modules/soju.nix
@@ -11,7 +11,7 @@
 
       enable = true;
       hostName = "orac";
-      listen = [ "irc+insecure://0.0.0.0" ];
+      listen = [ "irc+insecure://0.0.0.0:6697" ];
       enableMessageLogging = true;
     };
   };
