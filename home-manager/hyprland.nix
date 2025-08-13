250696a3d2d71574852d152df82f7949c9b54544
death916 <mail@trentnelson.dev>
2025-08-13 02:22:41 -0700

    theme

--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -455,13 +455,13 @@
   gtk = {
     enable = true;
     theme = {
-    name = "Materia-dark";
-    package = pkgs.materia-theme;
-     # name = "Catppuccin-Mocha-Standard-Lavender-Dark";
+      name = "Materia-dark";
+      package = pkgs.materia-theme;
+      # name = "Catppuccin-Mocha-Standard-Lavender-Dark";
       #package = pkgs.catppuccin-gtk.override {
-       # accents = [ "lavender" ];
-        #size = "standard";
-        v#ariant = "mocha";
+      # accents = [ "lavender" ];
+      #size = "standard";
+      #variant = "mocha";
       #};
     };
     iconTheme = {
@@ -498,5 +498,6 @@
     libnotify
     kdePackages.dolphin
     catppuccin-gtk
+    materia-theme
   ];
 }
