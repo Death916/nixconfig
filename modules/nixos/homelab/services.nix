aabb0804cf542ef3d97c687b748f30aa7364ea56
death916 <mail@trentnelson.dev>
2025-08-19 02:00:26 -0700

    bit

--- modules/nixos/homelab/services.nix
+++ modules/nixos/homelab/services.nix
@@ -31,18 +31,18 @@
     port = 13378;
   };
 
-  #  users.users.qbittorrent.extraGroups = [ "media_services" ];
-#  users.groups.qbittorrent = { };
-#
-#  services.qbittorrent = {
-#    enable = true;
-#    dataDir = "/storage/services/qbittorrent";
-#    user = "qbittorrent";
-#    group = "qbittorrent";
-#    port = 8090;
-#    openFirewall = true;
-#    package = pkgs.qbittorrent-nox;
-#  };
+  users.users.qbittorrent.extraGroups = [ "media_services" ];
+  #  users.groups.qbittorrent = { };
+  #
+  #  services.qbittorrent = {
+  #    enable = true;
+  #    dataDir = "/storage/services/qbittorrent";
+  #    user = "qbittorrent";
+  #    group = "qbittorrent";
+  #    port = 8090;
+  #    openFirewall = true;
+  #    package = pkgs.qbittorrent-nox;
+  #  };
 
   systemd.tmpfiles.rules = [
     "d /storage/services/qbittorrent 0755 qbittorrent qbittorrent - -"
