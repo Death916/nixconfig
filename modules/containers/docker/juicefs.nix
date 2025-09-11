9cfd8571c51723f6014040a3ab6e71558e434eb9
death916 <mail@trentnelson.dev>
2025-09-11 05:02:20 -0700

    fix juicefs db cmd

--- modules/containers/docker/juicefs.nix
+++ modules/containers/docker/juicefs.nix
@@ -67,7 +67,7 @@
     ];
     log-driver = "journald";
     extraOptions = [
-      "--health-cmd=sh -c 'pg_isready -U $POSTGRES_USER -d $POSTGRES_DB'"
+      "      "--health-cmd=sh -c 'pg_isready -U death916 -d juicefs'""
       "--health-interval=10s"
       "--health-retries=5"
       "--health-timeout=5s"
