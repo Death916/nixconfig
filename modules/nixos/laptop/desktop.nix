3686ee77dc88c5bfa0b8265355907068729a4f4d
death916 <mail@trentnelson.dev>
2025-09-17 05:10:42 -0700

    portal back to hypr

--- modules/nixos/laptop/desktop.nix
+++ modules/nixos/laptop/desktop.nix
@@ -28,8 +28,8 @@
     enable = true;
     xwayland.enable = true;
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-    portalPackage =
-      inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+    # portalPackage =
+    #   inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
 
   environment.systemPackages = with pkgs; [
