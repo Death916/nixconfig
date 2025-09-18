b88e1b8556eec0e64de1e0dfdec057735b5b03b6
death916 <mail@trentnelson.dev>
2025-09-18 03:18:56 -0700

    qt

--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -484,14 +484,14 @@
 
     theme = {
       
-      # name = "Materia-dark";
-      # package = pkgs.materia-theme;
-      name = "Catppuccin-Mocha-Standard-Lavender-Dark";
-      package = pkgs.catppuccin-gtk.override {
-      accents = [ "lavender" ];
-      size = "standard";
-      variant = "mocha";
-      };
+       name = "Materia-dark";
+       package = pkgs.materia-theme;
+      #name = "Catppuccin-Mocha-Standard-Lavender-Dark";
+      #package = pkgs.catppuccin-gtk.override {
+      #accents = [ "lavender" ];
+      #size = "standard";
+      #variant = "mocha";
+      #};
     };
     iconTheme = {
       name = "Papirus-Dark";
