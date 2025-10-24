1b8c88255713db021b4b8945c6b2f4fc84ab0a84
death916 <mail@trentnelson.dev>
2025-10-24 01:52:51 -0700

    waveterm overlay

--- overlays/waveterm-overlay.nix
+++ overlays/waveterm-overlay.nix
@@ -1,6 +1,6 @@
 final: prev: {
   waveterm = prev.waveterm.overrideAttrs (oldAttrs: rec {
-    version = "0.12.0";
+    version = "0.12.1";
 
     src = prev.fetchurl {
       url = "https://github.com/wavetermdev/waveterm/releases/download/v${version}/waveterm-linux-amd64-${version}.deb";
