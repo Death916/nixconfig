2b1a4832fc3306bffe64970b5271e95930bda221
death916 <mail@trentnelson.dev>
2025-11-04 02:10:28 -0800

    waybar

--- flake.nix
+++ flake.nix
@@ -68,6 +68,7 @@
                 {
                   home-manager.useGlobalPkgs = true;
                   home-manager.useUserPackages = true;
+                  home-manager.backupFileExtension = "backup";
                   home-manager.extraSpecialArgs = { inherit hmLib unstablePkgs inputs; };
                   home-manager.users.death916 = {
                     imports = [
