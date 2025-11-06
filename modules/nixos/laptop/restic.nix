b842e2a7d0b55d0d78a499cfa2dbbbf5b39b001a
death916 <mail@trentnelson.dev>
2025-11-06 02:21:51 -0800

    restic

--- modules/nixos/laptop/restic.nix
+++ modules/nixos/laptop/restic.nix
@@ -1,80 +1,36 @@
-{ config, ... }:
+{ config, pkgs, ... }:
+
 {
-  # Restic secrets / passwords
-  sops.secrets = {
-  };
+  services.restic.enable = true;
+  services.restic.backups.laptop = {
+         user = "death916";
+         initialize = true;
+         #passwordFile
+         repository = "s3:backups.d8j2.or.idrivee2-38.com";
+         environmentFile = "/etc/nixos/secrets/restic.env";
+         paths = [
+           "/home/"
+           "/root"
+           "/var/lib/"
+           "/var/log/"
+           "/etc/"
 
-  services.restic.backups = {
-    laptop-backups = {
-      initialize = true;
-      paths = [
-        "/etc"
-        "/srv"
-        "/var/log"
-        "/home/"
-        "/var/lib/"
-        "/root/"
-        "/storage"
-      ];
-      repository = "s3:s3.us-west-000.backblazeb2.com";
-      environmentFile = config.sops.secrets."restic/B2-Backblaze/environment".path;
-      timerConfig = {
-        OnCalendar = "weekly";
-        Persistent = true;
-      };
-      extraBackupArgs = [
-        "--verbose"
-        "--exclude-caches"
-      ];
-      pruneOpts = [
-        "--keep-weekly 4"
-        "--keep-monthly 6"
-        "--keep-yearly 3"
-      ];
-    };
+            ];
+         pruneOpts = [
+           "--keep-hourly 48"
+           "--keep-daily 7"
+           "--keep-weekly 4"
+           "--keep-monthly 12"
+           "--keep-yearly 5"
+         ];
+         extraBackupArgs = [
+           "--verbose"
+           "--exclude-caches"
+         ];
+         timerConfig = {
+           OnCalendar = "daily";
+           Persistent = true;
+         };
+       };
 
-    home-folder = {
-      initialize = true;
-      paths = [ "/home/death916/Desktop" ];
-      repository = "b2:nuc-server-B2-Docker:/";
-      environmentFile = config.sops.secrets."restic/B2-Docker/environment".path;
-      passwordFile = config.sops.secrets."restic/B2-Docker/password".path;
-      timerConfig = {
-        OnCalendar = "daily";
-        Persistent = true;
-      };
-      extraBackupArgs = [
-        "--verbose"
-        "--exclude-caches"
-      ];
-      pruneOpts = [
-        "--keep-daily 7"
-        "--keep-weekly 8"
-        "--keep-monthly 6"
-        "--keep-yearly 1"
-      ];
-    };
-
-    B2-Photos = {
-      initialize = true;
-      paths = [ "/Storage/Media/Pictures" ];
-      repository = "b2:nuc-server-B2-Photos:/";
-      environmentFile = config.sops.secrets."restic/B2-Photos/environment".path;
-      passwordFile = config.sops.secrets."restic/B2-Photos/password".path;
-      timerConfig = {
-        OnCalendar = "weekly";
-        Persistent = true;
-      };
-      extraBackupArgs = [
-        "--verbose"
-        "--exclude-caches"
-      ];
-      pruneOpts = [
-        "--keep-weekly 8"
-        "--keep-monthly 12"
-        "--keep-yearly 5"
-      ];
-    };
-
-  };
 }
