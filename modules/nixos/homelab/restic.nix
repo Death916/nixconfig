c22ad73fd08453902f189dba0af9ad69520f5eb6
death916 <tavn1992@gmail.com>
2025-11-07 02:49:35 -0800

    restic homelab

--- modules/nixos/homelab/restic.nix
+++ modules/nixos/homelab/restic.nix
@@ -3,36 +3,36 @@
 {
 
   services.restic.backups.homelab = {
-         user = "root";
-         initialize = true;
-         passwordFile = "/etc/nixos/secrets/restic-auth";
-         repository = "s3:d8j2.or.idrivee2-38.com/backups";
-         environmentFile = "/etc/nixos/secrets/restic.env";
-         paths = [
-           "/home/"
-           "/root"
-           "/var/lib/"
-           "/var/log/"
-           "/etc/"
-           "/storage/"
+    user = "root";
+    initialize = true;
+    passwordFile = "/etc/nixos/secrets/restic-auth";
+    repository = "s3:d8j2.or.idrivee2-38.com/backups";
+    environmentFile = "/etc/nixos/secrets/restic.env";
+    paths = [
+      "/home/"
+      "/root"
+      "/var/lib/"
+      "/var/log/"
+      "/etc/"
+      "/storage/"
 
-            ];
-         pruneOpts = [
-           "--keep-hourly 48"
-           "--keep-daily 7"
-           "--keep-weekly 4"
-           "--keep-monthly 12"
-           "--keep-yearly 5"
-         ];
-         extraBackupArgs = [
-           "--verbose"
-           "--exclude-caches"
-         ];
-         timerConfig = {
-           OnCalendar = "00:05";
-           Persistent = true;
-           RandomizedDelaySec = "5h"
-         };
-       };
+    ];
+    pruneOpts = [
+      "--keep-hourly 48"
+      "--keep-daily 7"
+      "--keep-weekly 4"
+      "--keep-monthly 12"
+      "--keep-yearly 5"
+    ];
+    extraBackupArgs = [
+      "--verbose"
+      "--exclude-caches"
+    ];
+    timerConfig = {
+      OnCalendar = "00:05";
+      Persistent = true;
+      RandomizedDelaySec = "5h";
+    };
+  };
 
 }
