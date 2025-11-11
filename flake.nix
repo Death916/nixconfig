2925df7dd17a90a03dfcdf3265f882a8376d65a4
death916 <mail@trentnelson.dev>
2025-11-11 05:35:00 -0800

    change cache order

--- flake.nix
+++ flake.nix
@@ -43,16 +43,16 @@
     {
       nixConfig = {
         substituters = [
+          "https://cache.nixos.org/"
           "https://hyprland.cachix.org"
           "https://cache.flox.dev"
           "https://nix-community.cachix.org"
-          "https://cache.nixos.org/"
         ];
         trusted-public-keys = [
+          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
           "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
           "flox-store-public-0:8c/B+kjIaQ+BloCmNkRUKwaVPFWkriSAd0JJvuDu4F0="
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         ];
       };
 
