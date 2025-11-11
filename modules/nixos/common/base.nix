f9dd67e996aeab22029b5c09f7265737589e5b1f
death916 <mail@trentnelson.dev>
2025-11-11 07:29:51 -0800

    subs

--- modules/nixos/common/base.nix
+++ modules/nixos/common/base.nix
@@ -22,13 +22,13 @@
     "flakes"
   ];
 
-  nix.settings.substituters = [
+  nix.settings.extra-substituters = [
     "https://cache.nixos.org/"
     "https://hyprland.cachix.org"
     "https://cache.flox.dev"
     "https://nix-community.cachix.org"
   ];
-  nix.settings.trusted-public-keys = [
+  nix.settings.extra-trusted-public-keys = [
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
     "flox-store-public-0:8c/B+kjIaQ+BloCmNkRUKwaVPFWkriSAd0JJvuDu4F0="
