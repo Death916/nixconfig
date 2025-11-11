a4c315851d047ae991535a1b379d3d2a225ae728
death916 <mail@trentnelson.dev>
2025-11-11 05:06:44 -0800

    cache

--- modules/nixos/common/base.nix
+++ modules/nixos/common/base.nix
@@ -32,6 +32,7 @@
     "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
     "flox-store-public-0:8c/B+kjIaQ+BloCmNkRUKwaVPFWkriSAd0JJvuDu4F0="
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
   ];
 
   zramSwap.enable = true;
