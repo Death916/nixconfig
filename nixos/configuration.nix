8649a649af06644c83be579262ad380ed041a7e5
death916 <mail@trentnelson.dev>
2025-11-12 01:19:07 -0800

    cache

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -23,6 +23,16 @@
     ../modules/nixos/laptop/restic.nix
   ];
 
+   nix.settings.substituters = [
+    "https://hyprland.cachix.org"
+    "https://cache.flox.dev"
+    "https://nix-community.cachix.org"
+  ];
+  nix.settings.trusted-public-keys = [
+    "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+    "flox-store-public-0:8c/B+kjIaQ+BloCmNkRUKwaVPFWkriSAd0JJvuDu4F0="
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+];
   services.greetd = {
     enable = true;
     settings = {
