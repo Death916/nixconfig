fb0fa7afec3115a3bba63aaf1255b7d957d200c5
death916 <mail@trentnelson.dev>
2025-11-12 03:44:32 -0800

    files back

--- modules/nixos/common/base.nix
+++ modules/nixos/common/base.nix
@@ -22,6 +22,19 @@
     "flakes"
   ];
 
+  nix.settings.substituters = lib.mkForce [
+    "https://cache.nixos.org/"
+    "https://hyprland.cachix.org"
+    "https://cache.flox.dev"
+    "https://nix-community.cachix.org"
+  ];
+  nix.settings.trusted-public-keys = lib.mkForce [
+    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+    "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs="
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+  ];
+
 
   nix.settings.trusted-users = [ "root" "death916" ];
 
