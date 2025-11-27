3fa01d44bb9a6b6572c16769cea4cf163648f3da
death916 <mail@trentnelson.dev>
2025-11-27 03:04:47 -0800

    hypr

--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -6,40 +6,35 @@
 }:
 
 {
-  # Enable Hyprland and XWayland
   wayland.windowManager.hyprland = {
     enable = true;
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     xwayland.enable = true;
-    systemd.variables = [ "--all" ]; # Ensure environment variables are passed to systemd service # For XWayland applications
+    systemd.variables = [ "--all" ];
 
-    settings = { # This is the *correct* and *only* settings block
-      # Monitors
-      monitor = ",1920x1080,auto,1"; # Set primary monitor to 1080p
+    settings = {
+      monitor = ",1920x1080,auto,1";
 
-      # Execute on startup
       exec-once = [
         "dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
         "systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
-        "swww init &" # Wallpaper daemon
-        "waybar &" # Start Waybar
+        "swww init &"
+        "waybar &"
         "swww img /home/death916/Pictures/wallpapers/jameswebb1.jpg &"
-        "dunst &" # Start Dunst notification daemon
-        "nm-applet --indicator &" # NetworkManager applet
-        "blueman-applet &" # Bluetooth applet
-      ]; # The 'exec-once' list is the last attribute in this settings block. No semicolon needed here.
+        "dunst &"
+        "nm-applet --indicator &"
+        "blueman-applet &"
+      ];
 
-      # General settings
       general = {
         gaps_in = 5;
         gaps_out = 10;
         border_size = 2;
-        "col.active_border" = "rgba(cba6f7ee) rgba(1e66f5ee) 45deg"; # Catppuccin Lavender/Blue
-        "col.inactive_border" = "rgba(313244aa)"; # Catppuccin Surface0
-        layout = "dwindle"; # Or "master", "tile"
-      }; # Semicolon added here
+        "col.active_border" = "rgba(cba6f7ee) rgba(1e66f5ee) 45deg";
+        "col.inactive_border" = "rgba(313244aa)";
+        layout = "dwindle";
+      };
 
-      # Decoration
       decoration = {
         rounding = 10;
         fullscreen_opacity = 1.0;
@@ -47,14 +42,9 @@
           enabled = true;
           size = 3;
           passes = 3;
-        }; # Semicolon added here
-        # drop_shadow = true;
-        # shadow_range = 4;
-        # shadow_render_power = 3;
-        # "col.shadow" = "rgba(1a1a1aee)";
-      }; # Semicolon added here
+        };
+      };
 
-      # Animations
       animations = {
         enabled = true;
         bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
@@ -66,57 +56,45 @@
           "fade, 1, 7, default"
           "workspaces, 1, 6, default"
         ];
-      }; # Semicolon added here
+      };
 
-      # Input
       input = {
         kb_layout = "us";
         follow_mouse = 1;
         touchpad = {
           natural_scroll = true;
-        }; # Semicolon added here
-        sensitivity = 1.0; # 0.0 - 1.0, 1.0 = no modification
-      }; # Semicolon added here
+        };
+        sensitivity = 1.0;
+      };
 
-      # Dwindle layout (default)
       dwindle = {
-        pseudotile = true; # Master switch for pseudotiling. Enabling this means windows will not resize when moved into a master area.
-        force_split = 2; # Master switch for forcing splitting.
-      }; # Semicolon added here
+        pseudotile = true;
+        force_split = 2;
+      };
 
-      # Gestures
-      # gestures = {
-      #   workspace_swipe = true;
-      # };
-
-      # Keybindings
       bind = [
-        # Window management
         "SUPER, Q, killactive,"
         "SUPER, M, exit,"
         "SUPER, F, fullscreen,"
         "SUPER, Space, togglefloating,"
-        "SUPER, P, pseudo," # Master switch for pseudotiling.
-        "SUPER, J, togglesplit," # Master switch for splitting.
-        "SUPER, L, exec, hyprlock" # Lock the screen
+        "SUPER, P, pseudo,"
+        "SUPER, J, togglesplit,"
+        "SUPER, L, exec, hyprlock"
 
-        # Move focus
         "SUPER, left, movefocus, l"
         "SUPER, right, movefocus, r"
         "SUPER, up, movefocus, u"
         "SUPER, down, movefocus, d"
 
-        # Move windows
         "SUPER SHIFT, left, movewindow, l"
         "SUPER SHIFT, right, movewindow, r"
         "SUPER SHIFT, up, movewindow, u"
         "SUPER SHIFT, down, movewindow, d"
 
-        # Resize active window
         "SUPER SHIFT, right, resizeactive, 10 0"
         "SUPER SHIFT, left, resizeactive, -10 0"
         "SUPER SHIFT, down, resizeactive, 0 -10 0 -10"
-        # Workspaces
+
         "SUPER, 1, workspace, 1"
         "SUPER, 2, workspace, 2"
         "SUPER, 3, workspace, 3"
@@ -128,7 +106,6 @@
         "SUPER, 9, workspace, 9"
         "SUPER, 0, workspace, 10"
 
-        # Move window to workspace
         "SUPER SHIFT, 1, movetoworkspace, 1"
         "SUPER SHIFT, 2, movetoworkspace, 2"
         "SUPER SHIFT, 3, movetoworkspace, 3"
@@ -140,21 +117,11 @@
         "SUPER SHIFT, 9, movetoworkspace, 9"
         "SUPER SHIFT, 0, movetoworkspace, 10"
 
-        # Scroll through workspaces
         "SUPER, mouse_down, workspace, e+1"
         "SUPER, mouse_up, workspace, e-1"
 
-        # Window resizing
         "SUPER, R, submap, resize"
-        # "submap = resize"
-        # # "binde = , right, resizeactive, 10 0"
-        # # "binde = , left, resizeactive, -10 0"
-        # # "binde = , up, resizeactive, 0 -10"
-        # # "binde = , down, resizeactive, 0 10"
-        # "bind = , escape, submap, reset"
-        # "submap = reset"
 
-        # Multimedia keys
         ", XF86AudioMute, exec, ${pkgs.pamixer}/bin/pamixer --toggle-mute"
         ", XF86AudioRaiseVolume, exec, ${pkgs.pamixer}/bin/pamixer --increase 5"
         ", XF86AudioLowerVolume, exec, ${pkgs.pamixer}/bin/pamixer --decrease 5"
@@ -162,58 +129,50 @@
         ", XF86MonBrightnessUp, exec, ${pkgs.brightnessctl}/bin/brightnessctl set +5%"
         ", XF86MonBrightnessDown, exec, ${pkgs.brightnessctl}/bin/brightnessctl set 5%-"
 
-        # Application launchers
-        "SUPER, Return, exec, waveterm" # Terminal
-        "SUPER, T, exec, ghostty" # Wave Terminal
-        "SUPER, D, exec, rofi -show drun" # Rofi application launcher
+        "SUPER, Return, exec, waveterm"
+        "SUPER, T, exec, ghostty"
+        "SUPER, D, exec, rofi -show drun"
         "SUPER, A, exec, rofi -show window"
         "SUPER, W, exec, microsoft-edge"
-        "SUPER, E, exec, nautilus" # File manager
+        "SUPER, E, exec, nautilus"
         "SUPER, N, exec, dunstctl history-pop"
-        # Screenshots
-        "SUPER SHIFT, S, exec, bash -c \"grim -g '$(slurp)' - | tee ~/Pictures/screenshots/$(date +%s).png | wl-copy\"" # Screenshot selection to clipboard
-        "SUPER SHIFT, Print, exec, bash -c \"grim - | tee ~/Pictures/screenshots/$(date +%s).png | wl-copy\"" # Screenshot full screen to clipboard
+
+        "SUPER SHIFT, S, exec, bash -c \"grim -g '$(slurp)' - | tee ~/Pictures/screenshots/$(date +%s).png\""
+        "SUPER SHIFT, Print, exec, bash -c \"grim - | tee ~/Pictures/screenshots/$(date +%s).png\""
+      ];
+
+      windowrulev2 = [
+        "opacity 1.0 override 1.0 override, class:^(vlc)$"
+        "opacity 1.0 override 1.0 override, class:^(jellyfinmediaplayer)$"
+        "float, title:^(Picture-in-Picture)$"
+        "float, class:^(confirm)$"
+        "float, class:^(dialog)$"
+        "float, class:^(file_progress)$"
+        "float, class:^(confirmreset)$"
+        "float, class:^(makeinput)$"
+        "float, class:^(download)$"
+        "float, class:^(notification)$"
+        "float, class:^(error)$"
+        "float, class:^(pinentry)$"
+        "float, class:^(ssh-askpass)$"
+        "float, class:^(lxpolkit)$"
+        "float, class:^(thunar)$"
+        "float, class:^(pavucontrol)$"
+        "float, class:^(blueman-applet)$"
+        "float, class:^(nm-applet)$"
+        "noinitialfocus, class:^(nm-applet)$"
+        "noinitialfocus, class:^(blueman-applet)$"
       ];
     };
-    extraConfig = [
-      # Window rules
-      # Opacity rules for specific applications
-      "windowrule = opacity 1.0, class:^(vlc)$"
-      "windowrule = opacity 1.0, class:^(jellyfinmediaplayer)$"
-
-      # Floating rules
-      "windowrule = float, title:^(Picture-in-Picture)$"
-      "windowrule = float, class:^(confirm)$"
-      "windowrule = float, class:^(dialog)$"
-      "windowrule = float, class:^(file_progress)$"
-      "windowrule = float, class:^(confirmreset)$"
-      "windowrule = float, class:^(makeinput)$"
-      "windowrule = float, class:^(download)$"
-      "windowrule = float, class:^(notification)$"
-      "windowrule = float, class:^(error)$"
-      "windowrule = float, class:^(pinentry)$"
-      "windowrule = float, class:^(ssh-askpass)$"
-      "windowrule = float, class:^(lxpolkit)$"
-      "windowrule = float, class:^(thunar)$"
-      "windowrule = float, class:^(pavucontrol)$"
-      "windowrule = float, class:^(blueman-applet)$"
-      "windowrule = float, class:^(nm-applet)$"
-
-      # Other rules
-      "windowrule = nofocus, class:^(nm-applet)$"
-      "windowrule = nofocus, class:^(blueman-applet)$"
-    ];
   };
 
   xdg.portal = {
     enable = true;
-    extraPortals = with pkgs;
-      [
-        xdg-desktop-portal-gtk
-      ];
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+    ];
   };
 
-  # Waybar (status bar)
   programs.waybar = {
     enable = true;
     settings = {
@@ -251,16 +210,16 @@
         "hyprland/workspaces" = {
           format = "{icon}";
           format-icons = {
-            "1" = "󰎤"; # 1
-            "2" = "󰎧"; # 2
-            "3" = "󰎪"; # 3
-            "4" = "󰎭"; # 4
-            "5" = "󰎱"; # 5
-            "6" = "󰎳"; # 6
-            "7" = "󰎶"; # 7
-            "8" = "󰎹"; # 8
-            "9" = "󰎼"; # 9
-            "10" = "󰎿"; # 10
+            "1" = "󰎤";
+            "2" = "󰎧";
+            "3" = "󰎪";
+            "4" = "󰎭";
+            "5" = "󰎱";
+            "6" = "󰎳";
+            "7" = "󰎶";
+            "8" = "󰎹";
+            "9" = "󰎼";
+            "10" = "󰎿";
             "active" = "";
             "default" = "";
           };
@@ -317,10 +276,6 @@
       };
     };
     style = ''
-      /*
-      * Catppuccin Mocha
-      */
-
       @define-color rosewater #f5e0dc;
       @define-color flamingo #f2cdcd;
       @define-color pink #f5c2e7;
@@ -427,10 +382,9 @@
     '';
   };
 
-  # Rofi (application launcher)
   programs.rofi = {
     enable = true;
-    theme = "arthur"; # Use a Catppuccin theme for Rofi
+    theme = "arthur";
     extraConfig = {
       modi = "drun,run,ssh,window";
       show-icons = true;
@@ -442,7 +396,6 @@
     enable = true;
   };
 
-  # Dunst (notification daemon)
   services.dunst = {
     enable = true;
     settings = {
@@ -460,7 +413,7 @@
         padding = 8;
         horizontal_padding = 8;
         frame_width = 2;
-        frame_color = "#cba6f7"; # Catppuccin Lavender
+        frame_color = "#cba6f7";
         separator_color = "frame";
         word_wrap = true;
         ellipsize = "middle";
@@ -470,7 +423,6 @@
         transparency = 5;
         idle_threshold = 120;
         markup = "full";
-        # format = "<b>%s</b>\n%b";
         format = "<small>%a</small>\n<big><b>%s</b></big>\n%b";
         alignment = "left";
         bounce_freq = 0;
@@ -491,27 +443,24 @@
         show_indicators = true;
       };
       urgency_low = {
-        background = "#1e1e2e"; # Catppuccin Base
-        foreground = "#cdd6f4"; # Catppuccin Text
+        background = "#1e1e2e";
+        foreground = "#cdd6f4";
       };
       urgency_normal = {
-        background = "#1e1e2e"; # Catppuccin Base
-        foreground = "#cdd6f4"; # Catppuccin Text
+        background = "#1e1e2e";
+        foreground = "#cdd6f4";
       };
       urgency_critical = {
-        background = "#f38ba8"; # Catppuccin Red
-        foreground = "#1e1e2e"; # Catppuccin Base
+        background = "#f38ba8";
+        foreground = "#1e1e2e";
       };
     };
   };
 
-  # Wallpaper setter
   services.swww = {
     enable = true;
-    # You'll set the actual wallpaper in exec-once in Hyprland settings
   };
 
-  # GTK Theming
   gtk = {
     enable = true;
     gtk3 = {
@@ -527,15 +476,8 @@
     };
 
     theme = {
-
       name = "Materia-dark";
       package = pkgs.materia-theme;
-      #name = "Catppuccin-Mocha-Standard-Lavender-Dark";
-      #package = pkgs.catppuccin-gtk.override {
-      #accents = [ "lavender" ];
-      #size = "standard";
-      #variant = "mocha";
-      #};
     };
     iconTheme = {
       name = "Papirus-Dark";
@@ -548,17 +490,15 @@
     };
   };
 
-  # Qt Theming
   qt = {
     enable = true;
     platformTheme.name = "qt5gtk2";
   };
+
   home.sessionVariables = {
-    #QT_QPA_PLATFORMTHEME = "gtk";  # Required for Qt apps like VLC
     QT_STYLE_OVERRIDE = "qt5gtk2";
   };
 
-  # Fonts
   fonts.fontconfig.enable = true;
   home.packages = with pkgs;
     [
