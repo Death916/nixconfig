5e500e6cb42441a18e97a150036d75a2d169b3fc
death916 <mail@trentnelson.dev>
2025-11-27 03:40:01 -0800

    hypr

--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -142,7 +142,7 @@
       ];
 
       windowrule = [
-        "match:float, opacity 0.6"
+        "match:float, opacity 0.6 override 0.6 override"
         # "opacity 1.0 1.0,fullscreen:0"
         # "opacity 0.9 0.9,floating:0"
         # "opacity 0.6 0.6,floating:1"
