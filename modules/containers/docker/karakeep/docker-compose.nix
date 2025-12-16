6b3e0cf80cb464c825fe558660d4f7976467f4d4
death916 <mail@trentnelson.dev>
2025-12-16 04:12:18 -0800

    karakeep docker

--- modules/containers/docker/karakeep/docker-compose.nix
+++ modules/containers/docker/karakeep/docker-compose.nix
@@ -101,7 +101,7 @@
       "KARAKEEP_VERSION" = "release";
       "MEILI_ADDR" = "http://meilisearch:7700";
       "NEXTAUTH_URL" = "http://localhost";
-      # "OPENAI_BASE_URL" = "https://generativelanguage.googleapis.com/v1beta";
+      "OPENAI_BASE_URL" = "https://generativelanguage.googleapis.com/v1beta";
     };
     environmentFiles = [
       /etc/nixos/secrets/karakeep.env
