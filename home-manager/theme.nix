f81d095f990718302abda8c5e2b6dc473b9e8500
death916 <mail@trentnelson.dev>
2025-12-24 05:02:09 -0800

    stylix new theme folder

--- home-manager/theme.nix
+++ home-manager/theme.nix
@@ -1,27 +1,12 @@
 { config, pkgs, inputs, ... }:
 
 {
+  imports = [
+    ./themes/ferra.nix
+  ];
+
   stylix.enable = true;
   stylix.polarity = "dark";
-
-  stylix.base16Scheme = {
-    base00 = "#0F0F19"; # Main Background
-    base01 = "#181824"; # Lighter Background
-    base02 = "#202034"; # Selection Background
-    base03 = "#8C8CB8"; # Comments / Dim
-    base04 = "#8C8CB8"; # Secondary Foreground
-    base05 = "#EDEDFE"; # Main Foreground
-    base06 = "#F8F8FF"; # Lighter Foreground
-    base07 = "#EDEDFE"; # Main Foreground (as white)
-    base08 = "#FF3366"; # Red
-    base09 = "#FFEA00"; # Orange / Yellow
-    base0A = "#FFEA00"; # Yellow
-    base0B = "#00F59B"; # Green
-    base0C = "#3CFFED"; # Cyan
-    base0D = "#33A1FF"; # Blue
-    base0E = "#FF66F6"; # Magenta
-    base0F = "#FF66F6"; # Violet / Magenta
-  };
 
   stylix.cursor = {
     package = pkgs.catppuccin-cursors;
