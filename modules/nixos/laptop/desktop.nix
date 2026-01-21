576db16eb7743bf62850116bf5557e1fa73ca806
death916 <mail@trentnelson.dev>
2026-01-21 01:26:15 -0800

    bluetooth

--- modules/nixos/laptop/desktop.nix
+++ modules/nixos/laptop/desktop.nix
@@ -99,6 +99,12 @@
   virtualisation.docker.enable = true;
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
+  services.blueman.enable = true;
+  hardware.bluetooth.settings = {
+    General = {
+      Experimental = true;
+    };
+  };
 
   programs.nh = {
     enable = true;
