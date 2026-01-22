1d41b435a59378efbed07dfc07b506ae8763d06d
death916 <tavn1992@gmail.com>
2026-01-22 05:38:17 -0800

    haos

--- nixos/homelab.nix
+++ nixos/homelab.nix
@@ -3,11 +3,7 @@
   imports = [
     ../modules/nextcloud-setup.nix
     ../modules/media/arr-suite.nix
-    #  ../modules/home-assistant.nix
-    ../modules/vms/homeassistant-vm.nix
-    ../modules/vms/incus-base.nix
     ../modules/smb.nix
-    #    ../modules/opencloud.nix
     ../modules/nixos/homelab/networking.nix
     ../modules/nixos/homelab/services.nix
     ../modules/nixos/common/base.nix
