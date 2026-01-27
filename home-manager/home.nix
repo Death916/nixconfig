c200fd8cf563b7f6a1e6f3c64609730b399377cf
death916 <mail@trentnelson.dev>
2026-01-27 05:41:40 -0800

    pokemon-colorscripts

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -103,6 +103,7 @@ in
     newsboat
     hypnotix
     obsidian
+    pokemon-colorscripts
   ];
 
   programs.ghostty = {
@@ -201,6 +202,7 @@ in
     enableCompletion = true;
     bashrcExtra = ''
       export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
+      pokemon-colorscripts -r
     '';
     shellAliases = {
       k = "kubectl";
