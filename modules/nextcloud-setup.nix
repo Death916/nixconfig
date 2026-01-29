4dbfa87e07a73e3060405d640d944d39b0dd71c2
death916 <tavn1992@gmail.com>
2026-01-29 01:39:31 -0800

    nc

--- modules/nextcloud-setup.nix
+++ modules/nextcloud-setup.nix
@@ -65,7 +65,26 @@ in
       session_keepalive = true;
       remember_login_cookie_lifetime = 7776000;
     };
-
+    settings.enabledPreviewProviders = [
+      "OC\\Preview\\BMP"
+      "OC\\Preview\\GIF"
+      "OC\\Preview\\JPEG"
+      "OC\\Preview\\Krita"
+      "OC\\Preview\\MarkDown"
+      "OC\\Preview\\MP3"
+      "OC\\Preview\\OpenDocument"
+      "OC\\Preview\\PNG"
+      "OC\\Preview\\TXT"
+      "OC\\Preview\\XBitmap"
+      "OC\\Preview\\Movie"
+      "OC\\Preview\\MSOffice2003"
+      "OC\\Preview\\MSOffice2007"
+      "OC\\Preview\\MSOfficeDoc"
+      "OC\\Preview\\PDF"
+      "OC\\Preview\\Photoshop"
+      "OC\\Preview\\SVG"
+      "OC\\Preview\\YML"
+    ];
     settings = {
       trusted_domains = [
         nextcloudExternalDomain
