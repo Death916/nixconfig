630a71c18980dd77035714ca0a41628d916fabbe
death916 <mail@trentnelson.dev>
2026-02-04 02:25:56 -0800

    theme

---
+++ home-manager/themes/gruv_hard.nix
@@ -0,0 +1,20 @@
+{
+  stylix.base16Scheme = {
+    base00 = "#1D2021"; # Background
+    base01 = "#3C3836"; # Lighter Background
+    base02 = "#504945"; # Selection Background
+    base03 = "#665C54"; # Comments (Matches BrightBlack)
+    base04 = "#BDAE93"; # Dark Foreground
+    base05 = "#D5C4A1"; # Foreground
+    base06 = "#EBDBB2"; # Lighter Foreground
+    base07 = "#FBF1C7"; # Lightest Foreground (Matches BrightWhite)
+    base08 = "#FB4934"; # Red
+    base09 = "#FE8019"; # Orange
+    base0A = "#FABD2F"; # Yellow
+    base0B = "#B8BB26"; # Green
+    base0C = "#8EC07C"; # Cyan
+    base0D = "#83A598"; # Blue
+    base0E = "#D3869B"; # Magenta
+    base0F = "#D65D0E"; # Brown
+  };
+}
