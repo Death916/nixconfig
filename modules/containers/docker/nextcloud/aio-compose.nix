b5fb58d9c4938774e51ef7c8f6ef85a7fa647bd2
death916 <tavn1992@gmail.com>
2026-02-07 01:28:11 -0800

    volume

--- modules/containers/docker/nextcloud/aio-compose.nix
+++ modules/containers/docker/nextcloud/aio-compose.nix
@@ -34,7 +34,7 @@ in
       APACHE_IP_BINDING = "0.0.0.0";
     };
     volumes = [
-      "${dockerBaseDir}/aio-config:/mnt/docker-aio-config"
+      "nextcloud_aio_mastercontainer_data:/mnt/docker-aio-config"
       "/var/run/docker.sock:/var/run/docker.sock:ro"
     ];
     extraOptions = [
