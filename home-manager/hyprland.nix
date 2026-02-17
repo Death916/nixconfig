ab081902e47300989f8db40223fcf488af476aac
death916 <mail@trentnelson.dev>
2026-02-17 00:56:52 -0800

    hypridle

--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -411,6 +411,36 @@
     enable = true;
   };
 
+  services.hypridle = {
+    enable = true;
+    settings = {
+      general = {
+        after_sleep_cmd = "hyprctl dispatch dpms on";
+        ignore_dbus_inhibit = false;
+        lock_cmd = "hyprlock";
+      };
+
+      listener = [
+        # Lock after 5 minutes of inactivity
+        {
+          timeout = 300;
+          on-timeout = "hyprlock";
+        }
+        # Turn off display after 10 minutes
+        {
+          timeout = 600;
+          on-timeout = "hyprctl dispatch dpms off";
+          on-resume = "hyprctl dispatch dpms on";
+        }
+        # Suspend the laptop after 30 minutes
+        {
+          timeout = 1800;
+          on-timeout = "systemctl suspend";
+        }
+      ];
+    };
+  };
+
   services.dunst = {
     enable = true;
     settings = {
