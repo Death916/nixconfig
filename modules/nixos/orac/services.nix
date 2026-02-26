1d6cbf03e5bbc1479af0a70198a60c366b9b8bb1
death916 <mail@trentnelson.dev>
2026-02-26 01:39:30 -0800

    orac adg

--- modules/nixos/orac/services.nix
+++ modules/nixos/orac/services.nix
@@ -70,50 +70,50 @@
     ];
   };
 
-  # services.adguardhome = {
-  #   enable = true;
-  #   openFirewall = true;
-  #   mutableSettings = true;
-  #   settings = {
-  #     http = {
-  #       address = "127.0.0.1:3000";
-  #     };
-  #     dns = {
-  #       # bind_hosts = [
-  #       # "192.168.0.116"
-  #       # "100.65.36.116"
-  #       # ];
-  #       port = 53;
-  #       upstream_dns = [
-  #         "9.9.9.9"
-  #         "8.8.8.8"
-  #       ];
-  #       bootstrap_dns = [
-  #         "9.9.9.9"
-  #         "8.8.4.4"
-  #       ];
-  #     };
-  #     filtering = {
-  #       protection_enabled = true;
-  #       filtering_enabled = true;
-  #       parental_enabled = false;
-  #       safe_search = {
-  #         enabled = false;
-  #       };
-  #     };
-  #     filters =
-  #       lib.map
-  #         (url: {
-  #           enabled = true;
-  #           url = url;
-  #         })
-  #         [
-  #           "https://adguardteam.github.io/HostlistsRegistry/assets/filter_9.txt"
-  #           "https://adguardteam.github.io/HostlistsRegistry/assets/filter_11.txt"
-  #           "https://adguardteam.github.io/HostlistsRegistry/assets/filter_1.txt"
-  #         ];
-  #   };
-  # };
+  services.adguardhome = {
+    enable = true;
+    openFirewall = true;
+    mutableSettings = true;
+    settings = {
+      http = {
+        address = "127.0.0.1:3000";
+      };
+      dns = {
+        # bind_hosts = [
+        # "192.168.0.116"
+        # "100.65.36.116"
+        # ];
+        port = 53;
+        upstream_dns = [
+          "9.9.9.9"
+          "8.8.8.8"
+        ];
+        bootstrap_dns = [
+          "9.9.9.9"
+          "8.8.4.4"
+        ];
+      };
+      filtering = {
+        protection_enabled = true;
+        filtering_enabled = true;
+        parental_enabled = false;
+        safe_search = {
+          enabled = false;
+        };
+      };
+      filters =
+        lib.map
+          (url: {
+            enabled = true;
+            url = url;
+          })
+          [
+            "https://adguardteam.github.io/HostlistsRegistry/assets/filter_9.txt"
+            "https://adguardteam.github.io/HostlistsRegistry/assets/filter_11.txt"
+            "https://adguardteam.github.io/HostlistsRegistry/assets/filter_1.txt"
+          ];
+    };
+  };
 
   environment.systemPackages = with pkgs; [
     git
