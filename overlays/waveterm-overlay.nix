8d266a448aa573e3d52a6c0b5085efe4c65ab18a
death916 <mail@trentnelson.dev>
2026-02-26 03:03:15 -0800

    Wave update

--- overlays/waveterm-overlay.nix
+++ overlays/waveterm-overlay.nix
@@ -4,7 +4,7 @@ final: prev: {
 
     src = prev.fetchurl {
       url = "https://github.com/wavetermdev/waveterm/releases/download/v${version}/waveterm-linux-amd64-${version}.deb";
-      sha256 = "";
+      sha256 = "sha256-YTPNs69Ss+fgk0n7sFbNMafueDrSNnri2Ndy3TyeLq4=";
     };
   });
 }
