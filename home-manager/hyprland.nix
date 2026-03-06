1d09558942a176b43fc34831176a8f5fc9ceae6b
death916 <mail@trentnelson.dev>
2026-03-06 07:30:21 -0800

    res

--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -17,9 +17,9 @@
 
     settings = {
       monitor = if (osConfig.networking.hostName == "death-pc") then [
-        "desc:Acer Technologies R240HY T4BAA0012400,1920x1080,-1920x0,1"
+        "desc:Acer Technologies R240HY T4BAA0012400,1920x1080,-2021x0,0.95"
         "desc:BNQ BenQ EL2870U 26M05467SL0,2560x1440,0x0,1"
-        "desc:WAM U24C 0000000000001,1920x1080,2560x0,1"
+        "desc:WAM U24C 0000000000001,1920x1080,2560x0,0.95"
       ] else [
         ",preferred,auto,1"
       ];
