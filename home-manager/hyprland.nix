e4d24042f8bc6dad90219aaa624e4d89174d440f
death916 <mail@trentnelson.dev>
2026-03-05 21:06:18 -0800

    res

--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -18,7 +18,7 @@
       monitor = [
         "HDMI-A-1,1920x1080,-1920x0,1"
         "DP-2,1920x1080,0x0,1"
-        "DP-1,1920x1080,1920x0,1"
+        "DP-1,2560x1440,1920x0,1"
         ",preferred,auto,1"
       ];
       "$fileManager" = "nautilus";
