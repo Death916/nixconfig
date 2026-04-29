28e70a8e51da68e9396207006d08236c80aa705c
death916 <mail@trentnelson.dev>
2026-04-29 03:29:18 -0700

    umami hostname

--- modules/umami.nix
+++ modules/umami.nix
@@ -5,6 +5,7 @@
     enable = true;
     createPostgresqlDatabase = true;
     settings = {
+      HOSTNAME = "0.0.0.0"
       PORT = 3010;
       APP_SECRET_FILE = "/etc/nixos/secrets/umami.env";
       DISABLE_TELEMETRY = true;
