78400b8d675dc6568a5ab877b495367264fa81ee
death916 <mail@trentnelson.dev>
2026-05-13 00:06:21 -0700

    rosepine

---
+++ home-manager/themes/rosepine.nix
@@ -0,0 +1,20 @@
+{
+  stylix.base16Scheme = {
+    base00 = "#191724"; # base
+    base01 = "#1f1d2e"; # surface
+    base02 = "#26233a"; # overlay
+    base03 = "#6e6a86"; # muted
+    base04 = "#908caa"; # subtle
+    base05 = "#e0def4"; # text
+    base06 = "#e0def4"; # text (brighter)
+    base07 = "#524f67"; # highlight low
+    base08 = "#eb6f92"; # love
+    base09 = "#f6c177"; # gold
+    base0A = "#f6c177"; # gold (bright yellow)
+    base0B = "#31748f"; # pine
+    base0C = "#9ccfd8"; # foam
+    base0D = "#c4a7e7"; # iris
+    base0E = "#c4a7e7"; # iris (magenta)
+    base0F = "#ebbcba"; # rose
+  };
+}
