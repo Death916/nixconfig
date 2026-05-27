3bb901b0fb57c65eedd2d9c1c31cb852c186e5d7
death916 <mail@trentnelson.dev>
2026-05-27 05:39:46 -0700

    theme

---
+++ home-manager/themes/poimandres.nix
@@ -0,0 +1,20 @@
+{
+  stylix.base16Scheme = {
+    base00 = "#1b1e28";
+    base01 = "#232735";
+    base02 = "#303545";
+    base03 = "#505770";
+    base04 = "#767c9d";
+    base05 = "#a6accd";
+    base06 = "#d0d0d0";
+    base07 = "#ffffff";
+    base08 = "#d0679d";
+    base09 = "#fffac2";
+    base0A = "#add7ff";
+    base0B = "#5de4c7";
+    base0C = "#89ddff";
+    base0D = "#add7ff";
+    base0E = "#fcc5e9";
+    base0F = "#373b41";
+  };
+}
