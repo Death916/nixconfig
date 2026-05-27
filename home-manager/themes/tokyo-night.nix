c7c346ea6e16ba3bfab6efb3045731e41323d97a
death916 <mail@trentnelson.dev>
2026-05-27 05:41:06 -0700

    theme

---
+++ home-manager/themes/tokyo-night.nix
@@ -0,0 +1,20 @@
+{
+  stylix.base16Scheme = {
+    base00 = "#24283b";
+    base01 = "#1a1b26";
+    base02 = "#343a52";
+    base03 = "#444b6a";
+    base04 = "#787c99";
+    base05 = "#787c99";
+    base06 = "#cbccd1";
+    base07 = "#d5d6db";
+    base08 = "#f7768e";
+    base09 = "#ff9e64";
+    base0a = "#e0af68";
+    base0b = "#41a6b5";
+    base0c = "#7dcfff";
+    base0d = "#7aa2f7";
+    base0e = "#bb9af7";
+    base0f = "#d18616";
+  };
+}
