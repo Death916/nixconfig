6142a84892c7ceadcc6710c10079c5039d4b3495
death916 <mail@trentnelson.dev>
2026-06-23 01:12:36 -0700

    waybar hide

--- home-manager/waybar.nix
+++ home-manager/waybar.nix
@@ -155,11 +155,17 @@
       }
     '' + lib.optionalString (osConfig.networking.hostName == "nix-asus") ''
       window#waybar {
+        background-color: transparent;
+        background: transparent;
+      }
+
+      window#waybar > box {
+        background-color: #${config.lib.stylix.colors.base00};
         opacity: 0.3;
         transition: opacity 0.25s ease-in-out;
       }
 
-      window#waybar:hover {
+      window#waybar:hover > box {
         opacity: 1.0;
       }
     '';
