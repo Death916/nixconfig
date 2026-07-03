85e610c4449bc5190839e9a360fde2867b74a14a
death916 <mail@trentnelson.dev>
2026-07-03 05:35:59 -0700

    back to hyprland too much cosmic crashes

--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -64,7 +64,6 @@
       exec-once = [
         "dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
         "systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
-        "awww-daemon &"
         "waybar &"
         "poweralertd &"
         "dunst &"
