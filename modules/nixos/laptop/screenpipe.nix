53056b6d59f9165ec70d45ce9bc8036eb224b232
death916 <mail@trentnelson.dev>
2026-07-16 03:13:10 -0700

    screenpipe fps

--- modules/nixos/laptop/screenpipe.nix
+++ modules/nixos/laptop/screenpipe.nix
@@ -18,7 +18,7 @@ in
     environment = {
       SCREENPIPE_API_KEY = "local-recall-key";
       SCREENPIPE_USE_NPU_OCR = "1";
-      LEMONADE_VISION_MODEL = "qwen3vl-it-4b-FLM";
+      LEMONADE_VISION_MODEL = "qwen3.5-2b-FLM";
       LEMONADE_API_URL = "http://127.0.0.1:13305/v1/chat/completions";
       LEMONADE_TIMEOUT_SECS = "90";
       LEMONADE_CTX_SIZE = "4096";
@@ -27,7 +27,7 @@ in
     serviceConfig = {
       # Runs natively, storing data in the default SSD path (~/.local/share/screenpipe/)
       # Disables audio for lightweight background OCR
-      ExecStart = "${screenpipe-pkg}/bin/screenpipe record --disable-audio --listen-on-lan --ignored-windows \"cosmic-launcher\" --ignored-windows \"cosmic-app-library\" --ignored-windows \"cosmic-app-list\" --ignored-windows \"cosmic-applet\" --ignored-windows \"cosmic-panel\" --ignored-windows \"cosmic-notifications\" --ignored-windows \"cosmic-osd\" --ignored-windows \"cosmic-workspaces\"";
+      ExecStart = "${screenpipe-pkg}/bin/screenpipe record --fps 0.2 --disable-audio --listen-on-lan --ignored-windows \"cosmic-launcher\" --ignored-windows \"cosmic-app-library\" --ignored-windows \"cosmic-app-list\" --ignored-windows \"cosmic-applet\" --ignored-windows \"cosmic-panel\" --ignored-windows \"cosmic-notifications\" --ignored-windows \"cosmic-osd\" --ignored-windows \"cosmic-workspaces\"";
       Restart = "always";
       RestartSec = 5;
     };
