375622b36dacd5dd5f8bbbb19eb088ff3b4e59db
death916 <mail@trentnelson.dev>
2026-07-25 03:58:23 -0700

    lua sux i should switch off hyprland

--- home-manager/hypr-lua.nix
+++ home-manager/hypr-lua.nix
@@ -109,11 +109,11 @@ in
 
       animation = [
         { leaf = "windows"; enabled = true; speed = 7; bezier = "myBezier"; }
-        { leaf = "windowsOut"; enabled = true; speed = 7; style = "popin 80%"; }
-        { leaf = "border"; enabled = true; speed = 10; }
-        { leaf = "borderangle"; enabled = true; speed = 8; }
-        { leaf = "fade"; enabled = true; speed = 7; }
-        { leaf = "workspaces"; enabled = true; speed = 6; }
+        { leaf = "windowsOut"; enabled = true; speed = 7; bezier = "default"; style = "popin 80%"; }
+        { leaf = "border"; enabled = true; speed = 10; bezier = "default"; }
+        { leaf = "borderangle"; enabled = true; speed = 8; bezier = "default"; }
+        { leaf = "fade"; enabled = true; speed = 7; bezier = "default"; }
+        { leaf = "workspaces"; enabled = true; speed = 6; bezier = "default"; }
       ];
 
       bind = [
