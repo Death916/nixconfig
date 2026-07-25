a1b2cf2b1a4993ceb7fb1a635e913f38d92da5f8
death916 <mail@trentnelson.dev>
2026-07-25 03:37:30 -0700

    dumb lua hypr dev

--- home-manager/hypr-lua.nix
+++ home-manager/hypr-lua.nix
@@ -62,7 +62,7 @@
         "NIXOS_OZONE_WL,1"
       ]);
 
-      exec-once = [
+      "exec-once" = [
         "dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
         "systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
         "waybar &"
