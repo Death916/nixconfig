a5e5ea97651f7271cec4a3e6b797585ef6d21533
death916 <mail@trentnelson.dev>
2026-07-25 03:35:15 -0700

    dumb lua hypr dev

--- home-manager/hypr-lua.nix
+++ home-manager/hypr-lua.nix
@@ -11,6 +11,7 @@
 {
   wayland.windowManager.hyprland = {
     enable = true;
+    configType = "lua";
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     xwayland.enable = true;
     systemd.variables = [ "--all" ];
