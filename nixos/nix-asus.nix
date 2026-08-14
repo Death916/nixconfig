9346a30dd592c9a5be394c4268a8588aeda6edea
death916 <mail@trentnelson.dev>
2026-08-14 00:47:17 -0700

    vpn

--- nixos/nix-asus.nix
+++ nixos/nix-asus.nix
@@ -129,7 +129,10 @@
       ipv4 = {
         method = "manual";
         addresses = "$MULLVAD_CLIENT_IP/32";
-        routes = "23.234.72.2/32 10.0.100.1";
+        dns = "10.0.100.1;10.0.100.2;10.64.0.1;";
+        dns-search = "~.;death;";
+        dns-priority = -50;
+        routing-rule1 = "priority 2500 to 10.0.100.0/24 table 254";
       };
       ipv6 = {
         method = "disabled";
