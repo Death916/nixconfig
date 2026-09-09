bd18c0211611e57174060f22e8cf8a18dc6af5eb
death916 <tavn1992@gmail.com>
2026-09-09 01:26:33 -0700

    orbit port

--- modules/containers/docker/bookorbit.nix
+++ modules/containers/docker/bookorbit.nix
@@ -19,7 +19,7 @@
         "/storage/services/bookorbit/postgres:/var/lib/postgresql/data:rw"
       ];
       ports = [
-        "127.0.0.1:5422:5432"
+        "0.0.0.0:5422:5432"
       ];
       extraOptions = [
         "--health-cmd=pg_isready -U \"$POSTGRES_USER\" -d \"$POSTGRES_DB\""
