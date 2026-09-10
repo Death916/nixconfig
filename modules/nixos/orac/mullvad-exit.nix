eb3396e70362efcee066c5d0431966cc3aeba568
death916 <mail@trentnelson.dev>
2026-09-10 04:44:31 -0700

    ts--mullvad

--- modules/nixos/orac/mullvad-exit.nix
+++ modules/nixos/orac/mullvad-exit.nix
@@ -33,7 +33,7 @@
 
       networking.wg-quick.interfaces.wg-mullvad = {
         address = [ "10.69.143.223/32" ];
-        dns = [ "100.64.0.3" ];
+        dns = [ "192.168.100.1" ];
         privateKeyFile = "/etc/wireguard/wg-mullvad.key";
         peers = [
           {
