fb562ac2586da6b482bd3ed08103cf68c4923b87
death916 <mail@trentnelson.dev>
2026-09-14 02:42:11 -0700

    stop pearl auto

--- nixos/death-pc.nix
+++ nixos/death-pc.nix
@@ -16,7 +16,7 @@
     ../modules/nixos/laptop/hyprland-deps.nix
     ../modules/nixos/desktop/gaming.nix
     ../modules/nixos/desktop/restic.nix
-    ../modules/containers/docker/pearl-miner.nix
+    # ../modules/containers/docker/pearl-miner.nix
   ];
 
   /*
